581ecc5710b68a2a0f9a397f61740d3ac66163c2
Hameeda1721 <hameedaravikala69@gmail.com>
2022-12-19 16:27:29 +0530

    Messages

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,17 +10,17 @@ public class Main {
     }
     public static class B extends A{
         public String meth(){
-            //super.meth();
+
             return "Method is overridden in Extendend class B";
         }
 
 
     }
     public static void main(String[] args){
-        B b = new B;
+        B b = new B();
         b.meth().super();
 
-        B b1 = new B;
+        B b1 = new B();
         b1.meth();
 
     }

1d8bff92f6e7360288da277e6db19a306c8de76b
Hameeda1721 <hameedaravikala69@gmail.com>
2022-12-19 16:34:06 +0530

    change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
 
     public static class A{
-        @Override
+
         public String meth(){
             return "Invoking method from class A";
         }
